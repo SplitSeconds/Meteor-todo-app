04c70187b746e17aed30b9cf1f48634d0df9e944
Nele Luepkes <nluepkes@gmail.com>
2019-04-19 15:51:57 +0200

    delete and checked functions

--- tutorial/simple-todos/imports/ui/body.js
+++ tutorial/simple-todos/imports/ui/body.js
@@ -1,9 +1,35 @@
 import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
+ 
+import './task.js';
 import './body.html';
  
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    console.log("event", event)
+    console.log("event target", event.target)
+    console.log("text", event.target.text)
+    console.log("value", event.target.text.value)
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    target.text.value = '';
   },
 });
